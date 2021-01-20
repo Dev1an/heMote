6e2d058f045499bb34716ba13a3391b2a3a0874d
Michel Dufaux <micheldufaux@mac.com>
2021-01-20 15:48:13 +0100

    caminfo update

--- examples/caminfo.js
+++ examples/caminfo.js
@@ -1,20 +1,15 @@
 const controller = require('../index.js')
 
 controller.discoverer.on('add', function(cam) {
-	// console.log(cam)
 	const camera = new controller.Camera(cam.address)
-	// console.log(camera)
-	// camera.sendCommand('movement','QID', function(error, response, body) {
-	// 	console.log(response.body)
-	// })
 	camera.batchInfo(function(error, response, body) {
-		var keyVal = response.body.split("\r\n")		  
+		var keyVal = response.body.split(/\r\n/)		  
 		var obj = {};
 		var i;
 		for (i in keyVal) {
 			if(keyVal[i].indexOf(':') > 0) {
 				// keyVal[i] = keyVal[i].split(":");
-				var splits = keyVal[i].match(/([^:]*):(.*)/);
+				var splits = keyVal[i].match(/([^]*):(.*)/);
 				splits.shift()
 				// console.log(splits)
 				obj[splits[0]] = splits[1];
